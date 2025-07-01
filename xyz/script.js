39bf0d0ee3828fab4b99ce59758374afd2dd5368
MD SIMNAN NEMAT <simnan2319@gmail.com>
2025-07-01 20:24:42 +0530

    new changes

--- xyz/script.js
+++ xyz/script.js
@@ -1,17 +1,17 @@
-function loco() {
+function loco(){
     gsap.registerPlugin(ScrollTrigger);
 
 // Using Locomotive Scroll from Locomotive https://github.com/locomotivemtl/locomotive-scroll
 
 const locoScroll = new LocomotiveScroll({
-  el: document.querySelector("#sub-main"),
+  el: document.querySelector("#main"),
   smooth: true
 });
 // each time Locomotive Scroll updates, tell ScrollTrigger to update too (sync positioning)
 locoScroll.on("scroll", ScrollTrigger.update);
 
-// tell ScrollTrigger to use these proxy methods for the "#sub-main" element since Locomotive Scroll is hijacking things
-ScrollTrigger.scrollerProxy("#sub-main", {
+// tell ScrollTrigger to use these proxy methods for the "#main" element since Locomotive Scroll is hijacking things
+ScrollTrigger.scrollerProxy("#main", {
   scrollTop(value) {
     return arguments.length ? locoScroll.scrollTo(value, 0, 0) : locoScroll.scroll.instance.scroll.y;
   }, // we don't have to define a scrollLeft because we're only scrolling vertically.
@@ -19,9 +19,8 @@ ScrollTrigger.scrollerProxy("#sub-main", {
     return {top: 0, left: 0, width: window.innerWidth, height: window.innerHeight};
   },
   // LocomotiveScroll handles things completely differently on mobile devices - it doesn't even transform the container at all! So to get the correct behavior and avoid jitters, we should pin things with position: fixed on mobile. We sense it by checking to see if there's a transform applied to the container (the LocomotiveScroll-controlled element).
-  pinType: document.querySelector("#sub-main").style.transform ? "transform" : "fixed"
+  pinType: document.querySelector("#main").style.transform ? "transform" : "fixed"
 });
-
 
 // each time the window updates, we should refresh ScrollTrigger and then update LocomotiveScroll. 
 ScrollTrigger.addEventListener("refresh", () => locoScroll.update());
@@ -30,52 +29,100 @@ ScrollTrigger.addEventListener("refresh", () => locoScroll.update());
 ScrollTrigger.refresh();
 
 }
+loco()
 
-loco();
 
-gsap.to("#sub-main>video", {
-  scrollTrigger: {
-    trigger: `#sub-main>video`,
-    start: `50% top`, // jab video viewport ke andar aaye
-    end: "bottom top",
-    scroller: `#sub-main`
-  },
-  onStart: () => {
-    const vid = document.querySelector("#sub-main>video");
-    vid.loop = true;
-    vid.muted = true;
-    vid.play().catch((e) => {
-      console.log("Video play nahi hua:", e);
-    });
-);
+gsap.to("#page>video",{
+    scrollTrigger:{
+        trigger:`#page>video`,
+        start:`2% top`,
+        end:`bottom top`,
+        scroller:`#main`
+    },
+    onStart:()=>{
+        document.querySelector("#page>video").play()
+    }
+})
 
- var tl = gsap.timeline({
-  scrollTrigger: {
-    trigger: "#page3",
-    start: `top center`,
-    scrub: 1,
-    scroller: "#sub-main",
-    markers: true,
-    pin: true
-  }
- })
- tl.to("#page3", {
-  top:`-50%`
- });
 
- var tl1 = gsap.timeline({
-  scrollTrigger: {
-    trigger: "#page2",
-    start: `top top`,
-    scrub: 1,
-    scroller: "#sub-main",
-    markers: true,
-    pin: true
-  }
- })
- tl1.to("#page2>h1", {
-  top:`-50%`
- });
+gsap.to("#page",{
+    scrollTrigger:{
+        trigger:`#page`,
+        start:`top top`,
+        end:`bottom top`,
+        scroller:`#main`,
+        pin:true
+    }
+})
+
+
+gsap.to("#page-bottom",{
+    scrollTrigger:{
+        trigger:`#page-bottom`,
+        start:`5% top`,
+        end:`bottom top`,
+        scroller:`#main`,
+        scrub:.5,
+    },
+    opacity:0
+})
+
+
+
+var tl = gsap.timeline({
+    scrollTrigger:{
+        trigger:`#page1`,
+        start:`top top`,
+        scrub:1,
+        scroller:`#main`,
+        pin:true
+    }
+})
+
+
+tl.to("#page1>h1",{
+    top:`-50%`
+})
+
+
+
+
+
+
+var tl1 = gsap.timeline({
+    scrollTrigger:{
+        trigger:`#page2`,
+        start:`top top`,
+        scrub:1,
+        scroller:`#main`,
+        pin:true
+    }
+})
+
+
+tl1.to("#page2>h1",{
+    top:`-50%`
+})
+
+
+
+
+var tl2 = gsap.timeline({
+    scrollTrigger:{
+        trigger:`#page4`,
+        start:`top top`,
+        scrub:1,
+        scroller:`#main`,
+        pin:true
+    }
+})
+
+
+tl2.to("#page4>#center-page4",{
+    top:`-50%`
+})
+
+
 
 function canvas(){
     const canvas = document.querySelector("#page7>canvas");
@@ -315,11 +362,11 @@ gsap.to(imageSeq, {
   ease: `none`,
   scrollTrigger: {
     scrub: 0.15,
-    trigger: `#page7`,
+    trigger: `#page7>canvas`,
     //   set start end according to preference
     start: `top top`,
     end: `600% top`,
-    scroller: `#sub-main`,
+    scroller: `#main`,
   },
   onUpdate: render,
 });
@@ -355,10 +402,169 @@ ScrollTrigger.create({
   trigger: "#page7>canvas",
   pin: true,
   // markers:true,
-  scroller: `#sub-main`,
+  scroller: `#main`,
 //   set start end according to preference
   start: `top top`,
-  end: `400% top`,
+  end: `600% top`,
 });
 }
 canvas();
+
+
+
+function canvas1(){
+  const canvas = document.querySelector("#page18>canvas");
+const context = canvas.getContext("2d");
+
+canvas.width = window.innerWidth;
+canvas.height = window.innerHeight;
+
+
+window.addEventListener("resize", function () {
+canvas.width = window.innerWidth;
+canvas.height = window.innerHeight;
+render();
+});
+
+function files(index) {
+var data = `
+.//Apple vision canvas images/Vision00001.png
+.//Apple vision canvas images/Vision00002.png
+.//Apple vision canvas images/Vision00003.png
+.//Apple vision canvas images/Vision00004.png
+.//Apple vision canvas images/Vision00005.png
+.//Apple vision canvas images/Vision00006.png
+.//Apple vision canvas images/Vision00007.png
+.//Apple vision canvas images/Vision00008.png
+.//Apple vision canvas images/Vision00009.png
+.//Apple vision canvas images/Vision00010.png
+.//Apple vision canvas images/Vision00011.png
+.//Apple vision canvas images/Vision00012.png
+.//Apple vision canvas images/Vision00013.png
+.//Apple vision canvas images/Vision00014.png
+.//Apple vision canvas images/Vision00015.png
+.//Apple vision canvas images/Vision00016.png
+.//Apple vision canvas images/Vision00017.png
+.//Apple vision canvas images/Vision00018.png
+.//Apple vision canvas images/Vision00019.png
+.//Apple vision canvas images/Vision00020.png
+.//Apple vision canvas images/Vision00021.png
+.//Apple vision canvas images/Vision00022.png
+.//Apple vision canvas images/Vision00023.png
+.//Apple vision canvas images/Vision00024.png
+.//Apple vision canvas images/Vision00025.png
+`;
+return data.split("\n")[index];
+}
+
+const frameCount = 25;
+
+const images = [];
+const imageSeq = {
+frame: 1,
+};
+
+for (let i = 0; i < frameCount; i++) {
+const img = new Image();
+img.src = files(i);
+images.push(img);
+}
+
+gsap.to(imageSeq, {
+frame: frameCount - 1,
+snap: "frame",
+ease: `none`,
+scrollTrigger: {
+  scrub: 0.15,
+  trigger: `#page18`,
+  //   set start end according to preference
+  start: `top top`,
+  end: `80% top`,
+  scroller: `#main`,
+},
+onUpdate: render,
+});
+
+images[1].onload = render;
+
+function render() {
+scaleImage(images[imageSeq.frame], context);
+}
+
+function scaleImage(img, ctx) {
+var canvas = ctx.canvas;
+var hRatio = canvas.width / img.width;
+var vRatio = canvas.height / img.height;
+var ratio = Math.max(hRatio, vRatio);
+var centerShift_x = (canvas.width - img.width * ratio) / 2;
+var centerShift_y = (canvas.height - img.height * ratio) / 2;
+ctx.clearRect(0, 0, canvas.width, canvas.height);
+ctx.drawImage(
+  img,
+  0,
+  0,
+  img.width,
+  img.height,
+  centerShift_x,
+  centerShift_y,
+  img.width * ratio,
+  img.height * ratio
+);
+}
+ScrollTrigger.create({
+
+trigger: "#page18",
+pin: true,
+// markers:true,
+scroller: `#main`,
+//   set start end according to preference
+start: `top top`,
+end: `80% top`,
+});
+}
+canvas1();
+
+
+
+var tl3 = gsap.timeline({
+  scrollTrigger:{
+      trigger:`#page21`,
+      start:`top top`,
+      scrub:1,
+      scroller:`#main`,
+      pin:true
+  }
+})
+
+
+tl3.to("#page21>#troff",{
+  opacity:0
+})
+
+var tl4 = gsap.timeline({
+  scrollTrigger:{
+      trigger:`#page22`,
+      start:`top top`,
+      scrub:1,
+      scroller:`#main`,
+      pin:true
+  }
+})
+
+
+tl4.to("#page22>#snroff",{
+  opacity:0
+})
+
+
+
+gsap.to("#page23>img",{
+  scrollTrigger:{
+    trigger:`#page23>img`,
+    start:`top bottom`,
+    end:`bottom 60%`,
+    scrub:.5,
+    scroller:`#main`
+  },
+  opacity:1
+})
